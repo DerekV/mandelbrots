6b7602ffe20a7b4ea8a7214caf0489f92303f25a
Derek VerLee <dlverlee@mtu.edu>
2011-11-12 15:58:05 -0500

    implement inner loop

--- lisp/2011.11.12/generator.lisp
+++ lisp/2011.11.12/generator.lisp
@@ -2,11 +2,20 @@
 
 
 ;; for now, since state generator really has is maxitors, we will simply make it that.
+;; (simulating a clos like object with a maxitor slot)
 (defun make-generator (&key maxiter)
   maxiter)
 
-;; unimplemented!
-(defun get-value-at-point (generator point)
-  0)
+(defun maxiter (generator)
+  generator)
 
+;; unimplemented!
+(defun get-value-at-point (generator C)
+  (loop for i below (maxiter generator)
+       for Z = C then (+ (* Z Z) C)
+       while (< (abs Z) 2.0)
+     finally (return i)))
+
+
+   
     
