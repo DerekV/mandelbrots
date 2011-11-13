64383e7b8086640c7232be74a4c4ddb68f87d6ef
Derek VerLee <dlverlee@mtu.edu>
2011-11-13 00:35:56 -0500

    cleanup, api tweak

--- lisp/2011.11.12/generator.lisp
+++ lisp/2011.11.12/generator.lisp
@@ -1,5 +1,8 @@
 (in-package #:com.derekv.mandelbrots.lisp.2011-11-12.generator)
 
+;; quick start :
+;; try this from repl
+;; (create-png :from #C(-0.8 -0.8) :to #C(0.3 0.3) :resolution 3004 :maxiters 254 :filepath "~/tmp/mand.png")
 
 ;; for now, since state generator really has is maxitors, we will simply make it that.
 ;; (simulating a clos like object with a maxitor slot)
@@ -35,23 +38,6 @@
        (setq imag-sq (* imagz imagz))
        (incf i))))
 
-;; (loop for i below (maxiter generator)
-;;    while (< (abs Z) 2.0)
-;;    do (+ (* Z Z) C)
-;;    finally (return i))))
-;; (defclass crawler () 
-;;   ((cmin
-;;     :initarg :from
-;;     :reader :from)
-;;    (cmax
-;;     :initarg :to
-;;     :reader :to)
-;;    (resolution
-;;     :initarg :resolution
-;;     :reader :resolution)))
-
-;; (defgeneric get-next-point (crawler))
-
 (defun make-crawler (&key from to resolution)
   (let ((increment (/ (- to from) (- resolution 1)))
 	(next from))
@@ -61,41 +47,23 @@
 	    (if (or (> (realpart current) (realpart to))
 		    (> (imagpart current) (imagpart to)))
 		(setf next nil)
-		(progn 
+		(progn
 		  (setf next (+ current increment))
 		  current)))))))
 
 (defun get-next-point (crawler)
   (funcall crawler))
-	    
-      
+
+
 (defun create-list-accross-line (from to resolution maxiters)
   (let ((c (make-crawler :from from :to to :resolution resolution))
 	(g (make-generator :maxiter maxiters)))
     (loop for point = (get-next-point c) then (get-next-point c) while point
        collect (get-value-at-point g point))))
 
-
-;; (defun create-set-in-memory 
-;;     (let ((row-crawler (make-crawler :from #C(-1.2 -1.2) :to #C(-1.2 1.5) :resolution 198)))
-;;       (loop for row = (get-next-point row-crawler) then (get-next-point row-crawler)
-;; 	 while row
-;; 	 collect (create-list-accross-line row (+ row #C(1.7 0.0)) 198 255))) )
-
-;; (let* ((png (make-instance 'zpng:png
-;; 			    :color-type :grayscale-alpha
-;; 			    :width 199
-;; 			    :height 199))
-;; 	(image (zpng:data-array png))
-;;        (rownum 0) (colnum 0))
-;;   (dolist (row data) (incf rownum) (setf colnum 0)
-;; 	  (dolist (item row) (incf colnum)
-;; 		  (setf (aref image colnum rownum 1) item)))
-;;   (zpng:write-png png "/tmp/mandelbrot.png"))
-
-(defun create-png (from to resolution maxiters)
-  (let* ((row-crawler (make-crawler 
-		       :from from 
+(defun create-png (&key from to resolution maxiters filepath)
+  (let* ((row-crawler (make-crawler
+		       :from from
 		       :to (complex (realpart from) (imagpart to))
 		       :resolution resolution))
 	 (g (make-generator :maxiter maxiters))
@@ -108,18 +76,18 @@
 	for row = #1=(get-next-point row-crawler) then #1#
 	for rownum from 0 upto resolution
 	while row
-	do (let ((col-crawler 
-		  (make-crawler 
+	do (let ((col-crawler
+		  (make-crawler
 		   :from row
 		   :to (complex (realpart to) (imagpart row))
 		   :resolution resolution)))
 	     (loop
 		for col = #2=(get-next-point col-crawler) then #2#
 		for colnum from 0 upto resolution
-		while col	
+		while col
 		do (setf (aref image colnum rownum 0)
-			 (- 255 
-			    (mod 
+			 (- 255
+			    (mod
 				(get-value-at-point g col)
 			     255))))))
-     (zpng:write-png png "/tmp/mandelbrot.png")))
+     (zpng:write-png png filepath)))
